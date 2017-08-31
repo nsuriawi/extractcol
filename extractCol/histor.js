420e60a6634b6920ada9dd35c33e461697ee7f83
Nathaniel <nsuriawi@ucsc.edu>
2017-08-31 14:48:38 -0700

    fixed domain

--- extractCol/histor.js
+++ extractCol/histor.js
@@ -1,6 +1,7 @@
 var horiz = true,
-	bar   = false; //no color bar is selected to start
-
+	bar   = false, //no color bar is selected to start
+	s;
+	
 var canvas = document.querySelector("canvas"),
     context = canvas.getContext("2d"),
     width = canvas.width,
@@ -29,8 +30,9 @@ function loaded() {
       //.call(brush.move, [[300, 130], [450, 160]]);
 }
 //Selection of area brushed (replacement of extent in previous d3 iteration)
-function brushed() {			
-  var s = d3.event.selection,
+function brushed() {	
+	s = d3.event.selection;
+/*   var s = d3.event.selection,
       x0 = s[0][0],
       y0 = s[0][1],
       dx = s[1][0] - x0,
@@ -44,21 +46,20 @@ function brushed() {
   if (dx && dy) {
 	//data for the "extent" in brush
     var data = context.getImageData(x0, y0, dx, dy).data;			
+	var cursor = Colors(255,32,32,255);//placeholder for user value
 	console.log(data.length);
+	
 	
 	var roundx = Math.floor(dx),
 		roundy = Math.floor(dy);
-	var pixa = reorder(roundx, roundy, data);
-	//console.log(colorScale.range);
-	
-	
-	//placeholder for user value
-	var cursor = Colors(255,32,32,255);
+		
+	var pixa = reorder(roundx, roundy, data); //need (reorder) this line to happen after brushended, the domain is being preset before user entry.
 	searchArray(roundx,roundy,pixa,cursor);
-  }
+	//console.log(colorScale.range);
+  } */
 }
 
-function brushended(dx,dy,data){
+function brushended(){
 	if(bar == false){
 		console.log("prompt appears here");
 		 var pMod = picoModal([
@@ -73,16 +74,61 @@ function brushended(dx,dy,data){
 			  parseFloat(modal.modalElem().childNodes[2].textContent),
 			  parseFloat(modal.modalElem().childNodes[4].textContent)
 			])
-			document.getElementById("scale_min").innerText = colorScale.domain()[0]
-			document.getElementById("scale_max").innerText = colorScale.domain()[1]
+			//document.getElementById("scale_min").innerText = colorScale.domain()[0]
+			//document.getElementById("scale_max").innerText = colorScale.domain()[1]
+			var d1 = document.getElementById("scale_min").innerText;
+			var d2 = document.getElementById("scale_max").innerText;
+			colorScale.domain[0] = d1;
+			colorScale.domain[1] = d2;
+			//console.log("The domain currently: ", colorScale.domain[0],", ", colorScale.domain[1]);
 		  })
 		  .afterClose(function(modal){
-			updateColorScale(brush.extent);
+			//document.getElementById("scale_min").innerText = colorScale.domain()[0]
+			//document.getElementById("scale_max").innerText = colorScale.domain()[1]
+			var d1 = document.getElementById("scale_min").innerText;
+			var d2 = document.getElementById("scale_max").innerText;
+			console.log("d1 = ", d1, "d2 = ", d2);
+			colorScale.domain[0] = d1;
+			colorScale.domain[1] = d2;
+			console.log("The domain ending with: ", colorScale.domain[0],", ", colorScale.domain[1]);
+			  //var s = d3.event.selection,
+			var	  x0 = s[0][0],
+				  y0 = s[0][1],
+				  dx = s[1][0] - x0,
+				  dy = s[1][1] - y0,
+				  max = 0;
+				  
+			  //checking whether bar is longer vertically or horizontally
+			  if(dx>=dy){horiz = true;}else{horiz =false;}		
+			  console.log("dx: ", dx, " dy: ", dy);
+			  
+			  //if (dx && dy) {
+				//data for the "extent" in brush
+				var data = context.getImageData(x0, y0, dx, dy).data;			
+				var cursor = Colors(255,32,32,255);//placeholder for user value
+				console.log(data.length);
+				
+				
+				var roundx = Math.floor(dx),
+					roundy = Math.floor(dy);
+					
+				var pixa = reorder(roundx, roundy, data); //need (reorder) this line to happen after brushended, the domain is being preset before user entry.
+				searchArray(roundx,roundy,pixa,cursor);
+				//console.log(colorScale.range);
+			  //}
 		  })
 		  .show()
-		  bar = true;
+		  //bar = true;
 	}
+	//Another method would be created and used to store color values when using brush the second time.
 }
+
+/*var Colors = function(red,green,blue){//,alpha){
+	this.red = red;
+	this.green = green;
+	this.blue = blue;
+		//"alpha" : alpha
+}*/
 
 
 var Colors = function(red,green,blue){//,alpha){
@@ -135,11 +181,29 @@ function reorder(dx,dy,data) {
 		//console.log(pixa);
 		//oneD is a single row selected to make searchArray faster
 		if(bar == false){
-			//updateColorScale(dx,dy,data);
 			var half = Math.floor(dy/2)
+			var colorRange = [];
+			var colorDomain = [];
 				for(var i=0;i<dx;i++){
+					var p =  pixa[i][half];
+					var hex = "#" + ("000000" + rgbToHex(p["red"], p["green"], p["blue"])).slice(-6);
+					colorDomain.push(colorScale.domain()[0]+((colorScale.domain()[1]-colorScale.domain()[0])/dx * i));
+					colorRange.push(hex);
 					oneD.push(pixa[i][half]);
 				}
+			colorScale.range(colorRange).domain(colorDomain);
+			console.log("Domain: ", colorDomain);
+			console.log(colorRange);
+			//colorScale.range(colorRange);
+			colorRange.forEach(function(d,i){
+				colorRef[d] = colorDomain[i];
+			})
+			if( colorRange.length > 2 ){
+				nearest = nearestColor.from( colorRange );
+			}
+			
+			console.log(colorScale(0.1));
+			bar = true;
 			return oneD;
 			
 		}
@@ -191,49 +255,7 @@ function rgbToHex(r, g, b) {
     return ((r << 16) | (g << 8) | b).toString(16);
 }
 
-
-function updateColorScale(extent){
-	console.log(extent);
-	var x0 = extent[0][0],
-		y0 = extent[0][1],
-		dx = extent[1][0] - x0,
-		dy = extent[1][1] - y0,
-		max = 0;
-	var data = context.getImageData(x0, y0, dx, dy).data;	
-	
-	var half = Math.floor(dy/2)
-	var colorRange = [];
-	var colorDomain = [];
-	var pixa = [];
-
-	for(var i=0;i<dx;i++){ 
-		pixa[i] = [];
-		for(var j=0;j<dy;j++){
-			var n = 4*i;
-			pixa[i][j] = Colors(data[(j*dx*4) + n],data[(j*dx*4) + n+1],data[(j*dx*4) + n+2]);//,data[(j*dx*4) + n+3]);
-		}
-	}
-		
-	for(var i=0;i<dx;i++){
-		var p =  pixa[i][half];
-		var hex = "#" + ("000000" + rgbToHex(p["red"], p["green"], p["blue"])).slice(-6);
-		colorDomain.push(colorScale.domain()[0]+((colorScale.domain()[1]-colorScale.domain()[0])/dx * i));
-		colorRange.push(hex);
-		oneD.push(pixa[i][half]);
-	}
-	colorScale.range(colorRange).domain(colorDomain);
-	console.log("Domain: ", colorDomain);
-	console.log(colorRange);
-	colorRange.forEach(function(d,i){
-		colorRef[d] = colorDomain[i];
-	})
-	if( colorRange.length > 2 ){
-		nearest = nearestColor.from( colorRange );
-	}
-}
-
 /**click to identify RGB value
-
 function convertNum(pixelData)
 {
 	var redRange = [255,0],
@@ -259,7 +281,6 @@ function convertNum(pixelData)
 	}
 	return t;
 }
-
 function findPos(obj) {
     var curleft = 0, curtop = 0;
     if (obj.offsetParent) {
@@ -277,7 +298,6 @@ function rgbToHex(r, g, b) {
         throw "Invalid color component";
     return ((r << 16) | (g << 8) | b).toString(16);
 }
-
 $('#example').click(function(e) {
     var pos = findPos(this);
     var x = e.pageX - pos.x;
@@ -310,6 +330,3 @@ $('#example').click(function(e) {
         .text(hex)
   });
   */
-
-
-
